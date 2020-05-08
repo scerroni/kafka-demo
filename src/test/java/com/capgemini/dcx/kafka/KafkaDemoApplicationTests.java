6337b9d3ab524fa6c5983a107cd16691b8e6f4e2
Stefano Cerroni <stefano.cerroni@MBP-di-Stefano.station>
2020-05-08 19:58:21 +0200

    first commit

---
+++ src/test/java/com/capgemini/dcx/kafka/KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.dcx.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
